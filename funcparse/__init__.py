1d21224943166e52cb84cffd40539b406ee2b06b
Seonu Jang <seonujang92@gmail.com>
2019-07-09 13:07:34 +0900

    fix func with one args return tuple as args

--- funcparse/__init__.py
+++ funcparse/__init__.py
@@ -15,6 +15,7 @@ def _parse_params(params: str) -> Tuple[tuple, dict]:
             args = ()
         else:
             args = eval(inner_params[: start_index])
+            args = tuple([args]) if not isinstance(args, tuple) else args
 
         kwargs = (eval('dict('+inner_params[start_index:]+')')
                   if start_index is not None else dict())
